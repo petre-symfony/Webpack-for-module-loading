b2e8a59775749ec96588168bb8a8c2dd0452e73c
petrero <petrero@gmail.com>
2018-11-03 21:37:41 +0200

    Tell Webpack to also look at login.js and build a second file.

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,12 @@
 const path = require('path');
 
 module.exports = {
-	entry: './public/js/rep_log.js',
+	entry: {
+		rep_log: './public/js/rep_log.js',
+		login: './public/js/login.js'
+	},
 	output: {
 		path: path.resolve(__dirname, 'public', 'build'),
-		filename: "rep_log.js"
+		filename: "[name].js"
 	}
 }
