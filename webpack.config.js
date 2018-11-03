fc9dd9b21c251d88db588b6807ff5b373a0da650
petrero <petrero@gmail.com>
2018-11-03 22:19:51 +0200

    Make a third entry in webpack config for layout.js Both tooltips are still kicking.
    This is wonderful. But, we still have a few issues. Like, both login.js and rep_log.js contain their own copies of jQuery. You can see the file sizes for these are huge. We're actually making our user download jQuery three times: once in each of these two files and a third time still via the script tag in our layout. Geez.
    
    That's something we will fix. I promise.

--- webpack.config.js
+++ webpack.config.js
@@ -3,7 +3,8 @@ const path = require('path');
 module.exports = {
 	entry: {
 		rep_log: './public/js/rep_log.js',
-		login: './public/js/login.js'
+		login: './public/js/login.js',
+		layout: './public/js/layout.js'
 	},
 	output: {
 		path: path.resolve(__dirname, 'public', 'build'),
