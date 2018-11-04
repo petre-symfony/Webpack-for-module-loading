5f4533ed02e1e1456c2085cb26251cd4afe5d327
petrero <petrero@gmail.com>
2018-11-04 17:18:31 +0200

    I just read this awesome article about how cool Sass is. So now, I'm bored with my old, main.css file. Oof. Ancient! Rename this to main.scss. Trendy! Exciting!
    Now that this is a Sass file - oooOOOooo - I'll re-work some syntax. Instead of having this extra :hover and :focus section, we can leverage Sass syntax and say &:hover, &:focus, followed by the CSS
    
    Now, what do you think will happen when Webpack sees this import? You can probably guess: it's not going to like it.
    
    Find your webpack terminal. Oh boy... oof, I was right. We have a very familiar error:
    
      Module parse failed. Unexpected token. You may need an appropriate loader to handle this file.
    
    We do have a loader for files ending in .css... but not .scss

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -4,7 +4,7 @@ const $ = require('jquery');
 require('bootstrap');
 require('bootstrap/dist/css/bootstrap.css');
 require('@babel/polyfill');
-require('../css/main.css');
+require('../css/main.scss');
 require('font-awesome/css/font-awesome.css');
 
 $(document).ready(function() {
