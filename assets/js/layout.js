6634fa9750d6e6c11fd284dec6a799a6b8c3a712
petrero <petrero@gmail.com>
2018-11-04 17:44:25 +0200

    Up until now, to load CSS or Sass, we've required it from JavaScript. But, you can also use @import from inside CSS. Close the node_modules/ directory and open our main.scss.
    Webpack parses these @import lines... so this is effectively identical to what we had before. Except... it doesn't work! Even PhpStorm is super angry! When you use require inside a JS file, Webpack knows to look for bootstrap-sass inside the node_modules/ directory. But when you use a CSS @import, that path is relative to this file.

--- assets/js/layout.js
+++ assets/js/layout.js
@@ -2,7 +2,6 @@
 
 const $ = require('jquery');
 require('bootstrap-sass');
-require('bootstrap-sass/assets/stylesheets/_bootstrap.scss');
 require('@babel/polyfill');
 require('../css/main.scss');
 require('font-awesome/css/font-awesome.css');
