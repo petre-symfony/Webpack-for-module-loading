4a416fc4d895e8ca153fe021aed14d39b8e320e1
petrero <petrero@gmail.com>
2018-11-05 12:54:06 +0200

    Use import everywhere

--- assets/js/Components/RepLogApp.js
+++ assets/js/Components/RepLogApp.js
@@ -1,10 +1,10 @@
 'use strict';
 
-const Helper = require('./RepLogAppHelper');
-const $ = require('jquery');
-const swal = require('sweetalert2');
-require('sweetalert2/dist/sweetalert2.css');
-require('./Routing');
+import Helper from './RepLogAppHelper';
+import $ from 'jquery';
+import swal from 'sweetalert2';
+import 'sweetalert2/dist/sweetalert2.css';
+import Routing from './Routing';
 import { random } from 'lodash/number';
 
 
--- assets/js/layout.js
+++ assets/js/layout.js
@@ -1,9 +1,9 @@
 'use strict';
 
-const $ = require('jquery');
-require('bootstrap-sass');
-require('@babel/polyfill');
-require('../css/main.scss');
+import $ from 'jquery';
+import 'bootstrap-sass';
+import '@babel/polyfill';
+import '../css/main.scss';
 
 $(document).ready(function() {
   $('[data-toggle="tooltip"]').tooltip();
