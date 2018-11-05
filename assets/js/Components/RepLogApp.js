8d6a28492545af7915780ef954a6d44f40407511
petrero <petrero@gmail.com>
2018-11-05 09:53:58 +0200

    We can't put Twig code in rep_log.js. For now, just to see if this is working, I'll paste two hard-coded logs here.
    In theory, this should work. Side note: you should always worry when a programmer says:
    
      In theory, this should work!
    
    But actually, it does this time! Sometimes we get lucky. The table starts with the two hard-coded logs.

--- assets/js/Components/RepLogApp.js
+++ assets/js/Components/RepLogApp.js
@@ -8,13 +8,14 @@ const swal = require('sweetalert2');
 let HelperInstances = new WeakMap();
 
 class RepLogApp {
-	constructor($wrapper) {
+	constructor($wrapper, initialRepLogs) {
 		this.$wrapper = $wrapper;
 		this.repLogs = [];
 		HelperInstances.set(this, new Helper(this.repLogs));
 
-		this.loadRepLogs();
-
+		for (let repLog of initialRepLogs) {
+			this._addRow(repLog);
+		}
 		this.$wrapper.on(
 			'click',
 			'.js-delete-rep-log',
@@ -37,17 +38,6 @@ class RepLogApp {
 		return {
 			newRepForm: '.js-new-rep-log-form'
 		}
-	}
-
-	loadRepLogs(){
-		$.ajax({
-			url: Routing.generate('rep_log_list'),
-		})
-		.then(data => {
-			for (let repLog of data.items) {
-				this._addRow(repLog);
-			};
-		});
 	}
 
 	handleRepLogDelete(e) {
--- assets/js/rep_log.js
+++ assets/js/rep_log.js
@@ -4,8 +4,25 @@ const RepLogApp = require('./Components/RepLogApp');
 
 global.$ = $;
 
+const logs = [
+	{
+		"links": {"_self": "\/reps\/78"},
+		"id": 78,
+		"reps": 1,
+		"itemLabel": "Big Fat Cat",
+		"totalWeightLifted": 18
+	},
+	{
+		"links": {"_self": "\/reps\/79"},
+		"id": 79,
+		"reps": 2,
+		"itemLabel": "Big Fat Cat",
+		"totalWeightLifted": 36
+	}
+];
+
 $(document).ready(function () {
 
 	var $wrapper = $('.js-rep-log-table');
-	var repLogApp = new RepLogApp($wrapper);
+	var repLogApp = new RepLogApp($wrapper, logs);
 });
