0c3c8761ab0acafd40dedfbb92bda4b67b9948bb
petrero <petrero@gmail.com>
2018-11-06 14:21:26 +0200

    Inside username_validation_error.js, that CSS is really a dependency of this module. So, add import '../../css/login-username-error.css
    When we refresh now, login.css does not contain that extra code. Yep, we've made that file slightly smaller. But when we try a really long username... it works! Look at the downloaded JavaScript file. Yes! It contains the CSS, down at the bottom.
    
    When the JS file loads, the CSS is being injected onto the page via a traditional style tag. Well, it's a weird blob actually... but conceptually, this is a style tag with that CSS.
    
    Oh, remember the extract-text-webpack-plugin?
    
     180 lines  webpack.config.js
    const ExtractTextPlugin = require('extract-text-webpack-plugin');
    const webpackConfig = {
        module: {
            rules: [
                {
                    test: /\.css$/,
                    use: ExtractTextPlugin.extract({
                        use: [
                            cssLoader
                        ],
                        // use this, if CSS isn't extracted
                        fallback: styleLoader
                    }),
                },
            ]
        },
    };
    Well, by default, it does not extract any CSS that has been code split. Nope, any code-split CSS is instead passed to the fallback loader: style-loader. In other words, code-split CSS is packaged into JavaScript and added to the page when that JavaScript file is downloaded.
    
    So next time you have some conditional code... think about code splitting: you could drastically reduce the size of your assets!

--- assets/js/Components/username_validation_error.js
+++ assets/js/Components/username_validation_error.js
@@ -1,4 +1,5 @@
 import $ from "jquery";
+import '../../css/login-username-error.css';
 
 export default function ($usernameInput){
 	const $warning = $('<div class="login-long-username-warning">This is a really long username - are you sure that is right?</div>');
